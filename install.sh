bca18bf2f8d97fd5f8fd90c1c94c24a00e8b93f3
Jeremy Ruppel <jeremy@coder.com>
2026-02-13 15:17:33 -0500

    brew shellenv in install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ sudo apt-get install build-essential procps curl file git
 # install linuxbrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# add brew to path
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 # install brew packages
 brew analytics off
 brew bundle
