a94e47684e9e683f5c58a08d50b20fe84f6c7de2
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2026-02-13 14:45:02 -0500

    brew shellenv

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# install dotfiles
+make
+
 # trying this with linuxbrew
 #
 # make sure dependencies are up to date
@@ -8,11 +11,12 @@ sudo apt-get install build-essential procps curl file git
 # install linuxbrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# install brew packages
-brew bundle
+# add brew to path
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
 
-# install dotfiles
-make
+# install brew packages
+which brew
+brew bundle
 
 # install vim plugins
 vi +PlugInstall +qall --headless
